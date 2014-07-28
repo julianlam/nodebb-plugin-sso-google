625a8bec2d8fad2fa04ee322b025eb5768cf19bb
Julian Lam <julian@designcreateplay.com>
2014-07-28 12:53:43 -0400

    0.5.0 compat

--- library.js
+++ library.js
@@ -20,13 +20,15 @@
 
 	var Google = {};
 
-	Google.init = function(app, middleware, controllers) {
+	Google.init = function(app, middleware, controllers, callback) {
 		function render(req, res, next) {
 			res.render('admin/plugins/sso-google', {});
 		}
 
 		app.get('/admin/plugins/sso-google', middleware.admin.buildHeader, render);
 		app.get('/api/admin/plugins/sso-google', render);
+
+		callback();
 	}
 
 	Google.getStrategy = function(strategies, callback) {
