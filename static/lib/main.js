8fb49171aabd4ad1a0655d118deb614ce18c6a69
Julian Lam <julian@nodebb.org>
2021-01-27 12:14:59 -0500

    fix: update login button replacement code to use new hook system

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,18 +1,12 @@
 'use strict';
 
-$(window).on('action:script.load', function (ev, data) {
-	data.scripts.push('sso-google/login');
+require(['hooks'], function (hooks) {
+	hooks.on('static:script.init', ({ tpl_url }) => {
+		if (tpl_url === 'login') {
+			var replaceEl = $('.alt-logins .google a i');
+			var replacement = document.createElement('img');
+			replacement.src = config.relative_path + '/plugins/nodebb-plugin-sso-google/images/btn_google_signin_' + config['sso-google'].style + '_normal_web.png';
+			replaceEl.replaceWith(replacement);
+		}
+	});
 });
-
-define('sso-google/login', function () {
-	var Login = {};
-
-	Login.init = function () {
-		var replaceEl = $('.alt-logins .google a i');
-		var replacement = document.createElement('img');
-		replacement.src = config.relative_path + '/plugins/nodebb-plugin-sso-google/images/btn_google_signin_' + config['sso-google'].style + '_normal_web.png';
-		replaceEl.replaceWith(replacement);
-	}
-
-	return Login;
-})
