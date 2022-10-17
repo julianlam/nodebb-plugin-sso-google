49682a926030243b446baf83efa92ec2a9b98471
Julian Lam <julian@nodebb.org>
2022-10-17 15:02:35 -0400

    chore: cleanup

--- static/lib/main.js
+++ static/lib/main.js
@@ -18,8 +18,6 @@ require(['hooks'], function (hooks) {
 
 			pictureEl.append(sourceEl);
 			pictureEl.append(fallbackEl);
-			// var replacement = document.createElement('img');
-			// replacement.src = config.relative_path + '/plugins/nodebb-plugin-sso-google/images/btn_google_signin_' + config['sso-google'].style + '_normal_web.png';
 			replaceEl.replaceWith(pictureEl);
 		}
 	});
