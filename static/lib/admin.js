13ae6df57664d8fcb3a9c1ff858109377aa255b8
Julian Lam <julian@nodebb.org>
2022-10-17 15:04:58 -0400

    chore: add eslint + lint

--- static/lib/admin.js
+++ static/lib/admin.js
@@ -1,23 +1,21 @@
 'use strict';
-/* globals $, app, socket, require */
 
-define('admin/plugins/sso-google', ['settings'], function(Settings) {
-
+define('admin/plugins/sso-google', ['settings'], function (Settings) {
 	var ACP = {};
 
-	ACP.init = function() {
+	ACP.init = function () {
 		Settings.load('sso-google', $('.sso-google-settings'));
 
-		$('#save').on('click', function() {
-			Settings.save('sso-google', $('.sso-google-settings'), function() {
+		$('#save').on('click', function () {
+			Settings.save('sso-google', $('.sso-google-settings'), function () {
 				app.alert({
 					type: 'success',
 					alert_id: 'sso-google-saved',
 					title: 'Settings Saved',
 					message: 'Please rebuild and restart your NodeBB to apply these settings, or click on this alert to do so.',
-					clickfn: function() {
+					clickfn: function () {
 						socket.emit('admin.reload');
-					}
+					},
 				});
 			});
 		});
@@ -25,7 +23,7 @@ define('admin/plugins/sso-google', ['settings'], function(Settings) {
 		$('a[data-action="help-credentials"]').on('click', function () {
 			bootbox.alert({
 				title: 'Where is the Credentials page?',
-				message: '<img src="' + config.relative_path + '/plugins/nodebb-plugin-sso-google/images/credentials.png" />'
+				message: '<img src="' + config.relative_path + '/plugins/nodebb-plugin-sso-google/images/credentials.png" />',
 			});
 			return false;
 		});
